84dfbb64f3273b440175c345423ecfa20ac7f501
Ryan Wang <wongyouth@gmail.com>
2019-04-05 18:09:31 +0800

    Add Rumbl.Auth

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -36,6 +36,25 @@ defmodule Rumbl.Accounts do
       ** (Ecto.NoResultsError)
 
   """
+  def get_user(id) do
+    Repo.get(User, id)
+    |> Repo.preload(:credential)
+  end
+
+  @doc """
+  Gets a single user.
+
+  Raises `Ecto.NoResultsError` if the User does not exist.
+
+  ## Examples
+
+      iex> get_user!(123)
+      %User{}
+
+      iex> get_user!(456)
+      ** (Ecto.NoResultsError)
+
+  """
   def get_user!(id) do
     Repo.get!(User, id)
     |> Repo.preload(:credential)
